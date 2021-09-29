560897553b7220d27b61a58f3d1be1450dfd9bb7
prrat <howto5349@gmail.com>
2021-09-29 19:15:29 +0100

    Updating the static files in settings.py

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -117,12 +117,9 @@ USE_TZ = True
 
 LOGIN_URL = 'login'
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles' )
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
-
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static')
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
